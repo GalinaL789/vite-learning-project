7f9d1b201419202a791914e70dfb36f25b1d9a45
Galina Lasko <galina_lasko@hotmail.com>
2024-03-18 18:23:04 +0100

    added one comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Homework29 from "./homeworks/Homework29"
 import { Homework29Wrapper } from "./homeworks/styles"
 
 function App() {
+  //comment
   return <Homework29Wrapper><Homework29/></Homework29Wrapper>  
 }
 
